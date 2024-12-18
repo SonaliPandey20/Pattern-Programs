6af7cfd2711de54645601ee3562b8879b3395a5f
SonaliPandey20 <sonalipandey202004@gmail.com>
2024-12-18 22:29:02 +0530

    Day-3 PatternPrograms

---
+++ PatternProgram12.java
@@ -0,0 +1,16 @@
+public class PatternProgram12 
+{
+    public static void main(String[] args) 
+    {
+        for(int i=1;i<=5;i++)
+        {
+            int k=i;
+            for(int j=1;j<=5;j++)
+            {
+                System.out.print( k + " " );
+                k=k+5;
+            }
+            System.out.println( " ");
+        }
+    }
+}
