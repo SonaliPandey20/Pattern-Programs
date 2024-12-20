d1b9dc6e6465cf009fe5574746d730d0726a341f
SonaliPandey20 <sonalipandey202004@gmail.com>
2024-12-20 18:50:39 +0530

    Daily Programs

---
+++ PatternProgram17.java
@@ -0,0 +1,15 @@
+public class PatternProgram17 
+{
+    public static void main(String[] args)
+    {
+        int n = 5;
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                System.out.print((2*(i+j))-3 + " ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ PatternProgram20.java
@@ -0,0 +1,22 @@
+public class PatternProgram20 
+{
+    public static void main(String[] args)
+    {
+        int n = 5;
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 0; j <= n; j++)
+            {
+                if(i%2==0)
+                {
+                    System.out.print("0" + " ");
+                }
+                else
+                {
+                    System.out.print(((i+j)%2) + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
